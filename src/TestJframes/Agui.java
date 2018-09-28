2c67e157f8278911ff1757ede630c16444989cde
tmp-sda-1157 <tmp-sda-1157@n171-p17.eduroam.kth.se>
2018-09-28 14:10:22 +0200

    first commit

---
+++ src/TestJframes/Agui.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+/**
+ * @author tmp-sda-1157
+ *
+ */
+
+package TestJframes;
+
+	import java.io.IOException;
+	import javax.swing.JFrame;
+
+
+public class Agui 
+{
+	
+	
+public static void main(String[] args) throws IOException
+{
+		TaskGUI taskgui = new TaskGUI();
+		taskgui.entryPoint();
+}
+}
